6c917da1db357db76de09b22454918573bcf30b9
Ryder <36494378+phoooey123@users.noreply.github.com>
2018-10-30 17:03:56 -0700

    Add files via upload
    Finished adding images fixed bug

---
+++ AlienDefenders.java
@@ -0,0 +1,26 @@
+package sorce;
+
+import javax.swing.JFrame;
+
+public class AlienDefenders {
+	public static final int width = 1000;
+	public static final int height = 800;
+
+	public static void main(String[] args) {
+		AlienDefenders ad = new AlienDefenders();
+
+	}
+
+	AlienDefenders() {
+		JFrame f = new JFrame();
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		GamePanel gp = new GamePanel();
+		f.add(gp);
+		f.addKeyListener(gp);
+		f.pack();
+		f.setSize(width, height);
+
+	}
+}
---
+++ GameObject.java
@@ -0,0 +1,16 @@
+package sorce;
+
+public class GameObject {
+	int x;
+	int y;
+	int width;
+	int height;
+
+	GameObject(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+}
